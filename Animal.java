af446a498cf4d8017ab87647a6e1f70d33e50f4d
Júlia <166644981+julialpsz@users.noreply.github.com>
2024-04-10 16:56:20 -0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,33 @@
+public class Animal {
+
+	private String nome;
+	private String tipo;
+
+	public Animal(String n, String t) {
+		this.nome = n;
+		this.tipo = t;
+		
+	}
+	public String getNome() {
+		return nome;
+		
+	}
+	public String getTipo() {
+		return tipo;
+		
+	}
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+		
+	}
+	public void setTipo(String novoTipo) {
+		this.tipo = novoTipo;
+		
+			
+	}
+	public void emitirSom() {
+		
+		
+	}
+
+}
