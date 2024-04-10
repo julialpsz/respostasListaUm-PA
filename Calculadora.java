eed6ba1a87817e811e362acd1044df4922166aaa
Júlia <166644981+julialpsz@users.noreply.github.com>
2024-04-10 16:53:11 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,22 @@
+public class Calculadora {
+		
+		private int a;
+		private int b;
+
+		public int soma (int a, int b) {
+			return a + b;
+		}
+		
+		public int subtrair (int a, int b) {
+			return a - b;
+		}
+		
+		public int mutiplicar(int a, int b) {
+			return a * b;
+		}
+		
+		public int dividir(int a, int b) {
+			return a / b;
+		}
+			
+	}
