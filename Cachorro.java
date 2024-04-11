d3afe508801826f4a08e7711d136653db658614a
Júlia <166644981+julialpsz@users.noreply.github.com>
2024-04-11 18:12:20 -0300

    Create Cachorro.java

---
+++ Cachorro.java
@@ -0,0 +1,42 @@
+public class Cachorro {
+
+	private String nome;
+	private String raca;
+	private int idade;
+	
+	public Cachorro(String n, String r, int i) {
+		this.nome = n;
+		this.raca = r;
+		this.idade = i;
+	}
+	
+	public String getNome() {
+	return nome;
+	}
+	
+	public String getRaca() {
+		return raca;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	
+	public void setRaca(String novaRaca) {
+		this.raca = novaRaca;	
+	}
+	
+	public void setIdade(String novaIdade) {
+		this.idade = novaIdade;
+	}
+	
+	public String latir() {	
+	return "au au!";
+	}
+	
+	
+}
