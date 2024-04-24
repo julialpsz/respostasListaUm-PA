340c17d22b36cb06cc9256a04b43ff1742374d8e
Júlia <166644981+julialpsz@users.noreply.github.com>
2024-04-24 11:19:29 -0300

    Create Gato.java

---
+++ Gato.java
@@ -0,0 +1,40 @@
+public class Gato {
+
+	private String nome;
+	private String raca;
+	private int idade;
+
+    public Gato(String n, String r, int i) {
+    	this.nome = n;
+    	this.raca = r;
+    	this.idade = i;
+    }
+    
+    public String getNome() {
+    	return nome;
+    }
+    
+    public String getRaca() {
+    	return raca;
+    }
+    
+    public int getIdade() {
+    	return idade;
+    }
+    
+    public void setNome(String novoNome) {
+    	this.nome = novoNome;
+    }
+    
+    public void setRaca(String novaRaca) {
+    	this.raca = novaRaca;
+    }
+    
+    public void setIdade (int novaIdade) {
+    	this.idade = novaIdade;
+    }
+    
+    public String miar() {
+    	return "miau!";
+    }
+}
