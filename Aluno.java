9a24c98a3f6c4e5f1b6682d17da81c1964e8ab8d
Júlia <166644981+julialpsz@users.noreply.github.com>
2024-04-24 11:40:15 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,30 @@
+public class Aluno {
+	private String nome;
+	private String matricula;
+	private String curso;
+	
+	public Aluno (String n, String m, String c) {
+		this.nome = n;
+		this.matricula = m;
+		this.curso = c;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setNome(String n) {
+		this.nome = n;
+	}
+	public void setMatricula(String m) {
+		this.matricula = m;
+	}
+	public void setCurso(String c) {
+		this.curso = c;
+	}
+}
